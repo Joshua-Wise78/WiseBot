ac2676cf19ba4cc588020b522c2b0306a556b542
Joshua Wise <joshua.wise722@gmail.com>
2026-02-27 08:34:42 -0500

    Paperless

---
+++ cogs/paperless.py
@@ -0,0 +1,10 @@
+import discord
+import httpx
+import os
+import io
+import uuid
+from datetime import datetime
+from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+
